e60a27fa4f99f87926ea8beb0c37fac9aee87982
bjornlecis <82440809+bjornlecis@users.noreply.github.com>
2021-12-22 20:39:15 +0100

    Create oefening3.py

---
+++ oefening3.py
@@ -0,0 +1,26 @@
+#oefening 3
+import queue
+
+q = queue.Queue(4)
+q.put({"cat": "kat"})
+q.put({"dog": "hond"})
+q.put({"horse": "paard"})
+q.put({"donkey": "ezel"})
+
+lijst = []
+
+for i in range(q.maxsize):
+    for (key,value) in q.get().items():
+        print(f'Welke dier is {key} in het Nederlands')
+        antwoord_gebruiker = input()
+
+        if antwoord_gebruiker == value:
+            lijst.append(f'Vraag {i+1}: juist')
+        else:
+            lijst.append(f'Vraag {i+1}: fout')
+        print('-------------------')
+
+print('')
+
+for item in lijst:
+    print(item)
